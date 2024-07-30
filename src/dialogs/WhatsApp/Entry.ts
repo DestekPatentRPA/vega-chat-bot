8d359c140abe25b49a103944f86840dd4686b4e9
Berk Ozerdogan <berkozerdogan@gmail.com>
2024-07-30 18:40:08 +0300

    v1.0.6

--- src/dialogs/WhatsApp/Entry.ts
+++ src/dialogs/WhatsApp/Entry.ts
@@ -14,15 +14,14 @@ export default class extends ServiceWhatsappBaseDialog implements IDialog {
 	// HASAN BEY TELEFON NUMARASI = 5305470851
 	async runDialog(): Promise<void> {
 		const userId = await getUserIdData(this.contact.phone.substring(2, 12));
-		const userName = await getUserInformation(userId[0].CustomerId);
-		this.conversation.setCache('userName', userName[0]['AD SOYAD']);
-
 		if (userId.length == 0 || userId.length > 1) {
 			await this.sendMessage(
 				'*‼️ Hata Mesajı* : Telefon numaranız sistemde kayıtlı değil veya birden fazla müşteri profili mevcut. Lütfen sistem yöneticisi ile görüşüp mevcut durumun düzeltilmesini talep ediniz.\n\n*🍃 Vega Gıda A.Ş. 🍃*',
 			);
 			throw new Error('end');
 		}
+		const userName = await getUserInformation(userId[0].CustomerId);
+		this.conversation.setCache('userName', userName[0]['AD SOYAD']);
 
 		this.conversation.setCache('customerId', userId[0].CustomerId);
 
