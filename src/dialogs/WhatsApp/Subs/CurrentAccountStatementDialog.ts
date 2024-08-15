a9b060ccd0f39c36acf3a5a25cd92db981f2c96f
Berk Ozerdogan <berkozerdogan@gmail.com>
2024-08-15 17:55:09 +0300

    v1.1.7

--- src/dialogs/WhatsApp/Subs/CurrentAccountStatementDialog.ts
+++ src/dialogs/WhatsApp/Subs/CurrentAccountStatementDialog.ts
@@ -51,7 +51,11 @@ export default class extends ServiceWhatsappBaseDialog implements IDialog {
 		const url = `${process.env.PUBLIC_URL}${this.contact.phone}.pdf`;
 		console.log('responseSendDocumentWithUrl' + url);
 		await new Promise((r) => setTimeout(r, 5000));
-		const a = await this.sendDocumentWithUrl('Cari Hesap Ekstresi', url);
+		const a = await this.sendDocumentWithUrl(
+			'Cari Hesap Ekstresi',
+			url,
+			'123123',
+		);
 		console.log(a);
 		await new Promise((r) => setTimeout(r, 120000));
 		await removePdfFromPath(result.outputPath);
