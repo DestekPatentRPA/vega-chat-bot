7641e850ecdc519d24439d601c1b16b8a99d9338
Berk Özerdoğan <berkozerdogan@gmail.com>
2024-10-08 12:37:24 +0300

    microsoft messages deleted

--- src/dialogs/Webchat/Entry.ts
+++ src/dialogs/Webchat/Entry.ts
@@ -1,11 +1,11 @@
 // Node modules.
 import {
 	IDialog,
-	ServiceMicrosoftBaseDialog,
+	ServiceWhatsappBaseDialog,
 	TBaseDialogCtor,
 } from 'cxperium-bot-engine';
 
-export default class extends ServiceMicrosoftBaseDialog implements IDialog {
+export default class extends ServiceWhatsappBaseDialog implements IDialog {
 	constructor(data: TBaseDialogCtor) {
 		super(data);
 	}
